cf8a86e55432627f60cbe7f3286871f6b36d9f61
WeldonCarlos <weldonprogrammer2022@gmail.com>
2024-10-07 16:21:09 -0300

    primeiro commit *estrutura do site*

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material"
+import { Header } from "../Header"
+
+export const Layout = () =>{
+    return(
+        
+        <Box>
+            <Header/>
+        </Box>
+    )
+}
